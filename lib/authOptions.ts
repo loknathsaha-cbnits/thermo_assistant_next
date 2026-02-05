8b95109bcaf0f90a0bbe695e13dcd3f563b9512b
adimanab <manab.das@cbnits.com>
2026-02-05 15:14:54 +0530

    removed oauth tables

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,11 +1,10 @@
 import { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import { prisma } from "@/lib/prisma";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
 
   providers: [
     CredentialsProvider({
